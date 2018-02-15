fd5960babb1919a103a644942071c7ed7ca3363b
Samuel Wirzén <samuel.wirzen@addinit.se>
2018-02-15 13:58:02 +0100

    Now supporting noptis transport authority conversion

---
+++ mobi.chouette.exchange.noptis/src/main/java/mobi/chouette/exchange/noptis/converter/NoptisTransportAuthorityConverter.java
@@ -0,0 +1,46 @@
+package mobi.chouette.exchange.noptis.converter;
+
+import mobi.chouette.common.Context;
+import mobi.chouette.exchange.noptis.Constant;
+import mobi.chouette.exchange.noptis.importer.Converter;
+import mobi.chouette.exchange.noptis.importer.ConverterFactory;
+import mobi.chouette.exchange.noptis.importer.NoptisImportParameters;
+import mobi.chouette.model.Company;
+import mobi.chouette.model.stip.TransportAuthority;
+import mobi.chouette.model.util.ObjectFactory;
+import mobi.chouette.model.util.Referential;
+
+public class NoptisTransportAuthorityConverter extends NoptisConverter implements Converter, Constant {
+
+    @Override
+    public void convert(Context context) throws Exception {
+        Referential referential = (Referential) context.get(REFERENTIAL);
+        NoptisImportParameters configuration = (NoptisImportParameters) context.get(CONFIGURATION);
+        TransportAuthority transportAuthority = (TransportAuthority) context.get(NOPTIS_DATA_CONTEXT);
+
+        String objectId = NoptisConverter.composeObjectId(configuration.getObjectIdPrefix(), Company.COMPANY_KEY, String.valueOf(transportAuthority.getGid()));
+        Company company = ObjectFactory.getCompany(referential, objectId);
+        company.setName(NoptisConverter.getNonEmptyTrimedString(transportAuthority.getFormalName()));
+        if (company.getName() == null)
+            company.setName(NoptisConverter.getNonEmptyTrimedString(transportAuthority.getName()));
+
+        // company.setUrl(NoptisConverter.toString(transportAuthority.getUrl()));
+        // company.setPhone(NoptisConverter.getNonEmptyTrimedString(transportAuthority.getPhone()));
+        String[] token = company.getObjectId().split(":");
+        company.setRegistrationNumber(token[2]);
+        // company.setTimeZone(NoptisConverter.toString(transportAuthority.getTimezone()));
+        company.setFilled(true);
+    }
+
+    static {
+        ConverterFactory.register(NoptisTransportAuthorityConverter.class.getName(), new ConverterFactory() {
+            private NoptisTransportAuthorityConverter instance = new NoptisTransportAuthorityConverter();
+
+            @Override
+            protected Converter create() {
+                return instance;
+            }
+        });
+    }
+
+}
