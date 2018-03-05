9533041c9fa2be69e44bda7177a8983533296b20
Samuel Wirzén <samuel.wirzen@addinit.se>
2018-03-05 15:46:49 +0100

    Add some debug logging to importer for incoming object id prefix and referential

--- mobi.chouette.exchange.noptis/src/main/java/mobi/chouette/exchange/noptis/importer/NoptisImporterCommand.java
+++ mobi.chouette.exchange.noptis/src/main/java/mobi/chouette/exchange/noptis/importer/NoptisImporterCommand.java
@@ -1,7 +1,6 @@
 package mobi.chouette.exchange.noptis.importer;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
 import com.jamonapi.Monitor;
 import com.jamonapi.MonitorFactory;
 import lombok.extern.log4j.Log4j;
@@ -29,75 +28,78 @@ import java.util.Map;
 @Stateless(name = NoptisImporterCommand.COMMAND)
 public class NoptisImporterCommand extends AbstractNoptisImporterCommand implements Command, Constant {
 
-	public static final String COMMAND = "NoptisImporterCommand";
+    public static final String COMMAND = "NoptisImporterCommand";
 
-	private static final Map<String, Short> DATA_SOURCES = ImmutableMap.of(
-			"OTR", (short)3, "ULA", (short)4, "VTK", (short)5, "SLT", (short)6);
+    private static final Map<String, Short> DATA_SOURCES = ImmutableMap.of(
+            "OTR", (short) 3, "ULA", (short) 4, "VTK", (short) 5, "SLT", (short) 6);
 
-	@Override
-	@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
-	public boolean execute(Context context) throws Exception {
-		boolean result = SUCCESS;
-		Monitor monitor = MonitorFactory.start(COMMAND);
+    @Override
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+    public boolean execute(Context context) throws Exception {
+        boolean result = SUCCESS;
+        Monitor monitor = MonitorFactory.start(COMMAND);
 
-		InitialContext initialContext = (InitialContext) context.get(INITIAL_CONTEXT);
-		ActionReporter actionReporter = ActionReporter.Factory.getInstance();
+        InitialContext initialContext = (InitialContext) context.get(INITIAL_CONTEXT);
+        ActionReporter actionReporter = ActionReporter.Factory.getInstance();
 
-		context.put(REFERENTIAL, new Referential());
+        context.put(REFERENTIAL, new Referential());
 
-		// initialize reporting and progression
-		ProgressionCommand progression = (ProgressionCommand) CommandFactory.create(initialContext,
-				ProgressionCommand.class.getName());
+        // initialize reporting and progression
+        ProgressionCommand progression = (ProgressionCommand) CommandFactory.create(initialContext,
+                ProgressionCommand.class.getName());
 
-		try {
+        try {
 
-		Object configuration = context.get(CONFIGURATION);
-		if (!(configuration instanceof NoptisImportParameters)) {
-			// fatal wrong parameters
-			log.error("invalid parameters for noptis import " + configuration.getClass().getName());
-			actionReporter.setActionError(context, ActionReporter.ERROR_CODE.INVALID_PARAMETERS, "invalid parameters for noptis import " + configuration.getClass().getName());
-			return false;
-		}
+            Object configuration = context.get(CONFIGURATION);
+            if (!(configuration instanceof NoptisImportParameters)) {
+                // fatal wrong parameters
+                log.error("invalid parameters for noptis import " + configuration.getClass().getName());
+                actionReporter.setActionError(context, ActionReporter.ERROR_CODE.INVALID_PARAMETERS, "invalid parameters for noptis import " + configuration.getClass().getName());
+                return false;
+            }
 
-		ProcessingCommands commands = ProcessingCommandsFactory.create(NoptisImporterProcessingCommands.class.getName());
-		result = process(context, commands, progression, true, Mode.line);
+            log.debug("OBJECT ID PREFIX IS : " + ((NoptisImportParameters) configuration).getObjectIdPrefix());
+            log.debug("REFERENTIAL NAME IS : " + ((NoptisImportParameters) configuration).getReferentialName());
 
-		} catch (CommandCancelledException e) {
-			actionReporter.setActionError(context, ActionReporter.ERROR_CODE.INTERNAL_ERROR, "Command cancelled");
-			log.error(e.getMessage());
-		} catch (Exception e) {
-			String fileName = (String) context.get(FILE_NAME);
-			log.error("Error parsing Noptis file "+fileName+": "+e.getMessage(), e);
-			actionReporter.setActionError(context, ActionReporter.ERROR_CODE.INTERNAL_ERROR, "Internal error while parsing Noptis files: "+e.toString());
-		} finally {
-			progression.dispose(context);
-			log.info(Color.YELLOW + monitor.stop() + Color.NORMAL);
-		}
+            ProcessingCommands commands = ProcessingCommandsFactory.create(NoptisImporterProcessingCommands.class.getName());
+            result = process(context, commands, progression, true, Mode.line);
 
-		return result;
-	}
+        } catch (CommandCancelledException e) {
+            actionReporter.setActionError(context, ActionReporter.ERROR_CODE.INTERNAL_ERROR, "Command cancelled");
+            log.error(e.getMessage());
+        } catch (Exception e) {
+            String fileName = (String) context.get(FILE_NAME);
+            log.error("Error parsing Noptis file " + fileName + ": " + e.getMessage(), e);
+            actionReporter.setActionError(context, ActionReporter.ERROR_CODE.INTERNAL_ERROR, "Internal error while parsing Noptis files: " + e.toString());
+        } finally {
+            progression.dispose(context);
+            log.info(Color.YELLOW + monitor.stop() + Color.NORMAL);
+        }
 
-	public static class DefaultCommandFactory extends CommandFactory {
+        return result;
+    }
 
-		@Override
-		protected Command create(InitialContext context) throws IOException {
-			Command result = null;
-			try {
-				String name = "java:app/mobi.chouette.exchange.noptis/" + COMMAND;
-				result = (Command) context.lookup(name);
-			} catch (NamingException e) {
-				String name = "java:module/" + COMMAND;
-				try {
-					result = (Command) context.lookup(name);
-				} catch (NamingException e1) {
-					log.error(e);
-				}
-			}
-			return result;
-		}
-	}
+    public static class DefaultCommandFactory extends CommandFactory {
 
-	static {
-		CommandFactory.factories.put(NoptisImporterCommand.class.getName(), new NoptisImporterCommand.DefaultCommandFactory());
-	}
+        @Override
+        protected Command create(InitialContext context) throws IOException {
+            Command result = null;
+            try {
+                String name = "java:app/mobi.chouette.exchange.noptis/" + COMMAND;
+                result = (Command) context.lookup(name);
+            } catch (NamingException e) {
+                String name = "java:module/" + COMMAND;
+                try {
+                    result = (Command) context.lookup(name);
+                } catch (NamingException e1) {
+                    log.error(e);
+                }
+            }
+            return result;
+        }
+    }
+
+    static {
+        CommandFactory.factories.put(NoptisImporterCommand.class.getName(), new NoptisImporterCommand.DefaultCommandFactory());
+    }
 }
