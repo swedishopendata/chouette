e2e841ce71f4221d51840fc795ede98626987076
Samuel Wirzén <samuel.wirzen@addinit.se>
2018-03-12 21:41:16 +0100

    Adds full runtime and test support for mssql server datasources

--- mobi.chouette.exchange.noptis/src/test/java/mobi/chouette/exchange/noptis/importer/NoptisImportIT.java
+++ mobi.chouette.exchange.noptis/src/test/java/mobi/chouette/exchange/noptis/importer/NoptisImportIT.java
@@ -100,7 +100,7 @@ public class NoptisImportIT extends Arquillian implements Constant, ReportConsta
 			}
 		}
 
-		final WebArchive testWar = ShrinkWrap.create(WebArchive.class, "test.war").addAsWebInfResource("postgres-ds.xml").addAsWebInfResource("mssql-ds.xml")//.addAsWebInfResource("h2-ds.xml")
+		final WebArchive testWar = ShrinkWrap.create(WebArchive.class, "test.war").addAsWebInfResource("postgres-ds.xml").addAsWebInfResource("h2-ds.xml")
 				.addClass(NoptisImportIT.class).addClass(NoptisTestUtils.class).addClass(DummyChecker.class).addClass(JobDataTest.class);
 
 		result = ShrinkWrap.create(EnterpriseArchive.class, "test.ear").addAsLibraries(jars.toArray(new File[0]))
