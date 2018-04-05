0de8b9d84e4626c84838002e31345a2ad44709ff
Samuel Wirzén <samuel.wirzen@addinit.se>
2018-04-05 15:18:55 +0200

    Fixes object prefix mappings

--- mobi.chouette.exchange.noptis/src/main/java/mobi/chouette/exchange/noptis/importer/util/NoptisImporterUtils.java
+++ mobi.chouette.exchange.noptis/src/main/java/mobi/chouette/exchange/noptis/importer/util/NoptisImporterUtils.java
@@ -7,14 +7,14 @@ import java.util.Map;
 public class NoptisImporterUtils {
 
     private static final Map<String, Short> DATA_SOURCES = ImmutableMap.of(
-            "OTR", (short) 5, "ULA", (short) 3, "VTK", (short) 14, "SLT", (short) 1);
+            "OTR", (short) 5, "KUL", (short) 3, "VTR", (short) 14, "SLT", (short) 1);
 
     public static short getDataSourceId(String objectIdPrefix) {
         return DATA_SOURCES.get(objectIdPrefix);
     }
 
     private static final Map<String, String> DATA_SOURCE_NAMES = ImmutableMap.of(
-            "OTR", "AB Östgötatrafiken", "ULA", "Kollektivtrafikförvaltningen UL", "VTK", "Västtrafik AB", "SLT", "Stockholms Lokaltrafik");
+            "OTR", "AB Östgötatrafiken", "KUL", "Kollektivtrafikförvaltningen UL", "VTR", "Västtrafik AB", "SLT", "Storstockholms Lokaltrafik");
 
     public static String getDataSourceName(String objectIdPrefix) {
         return DATA_SOURCE_NAMES.get(objectIdPrefix);
